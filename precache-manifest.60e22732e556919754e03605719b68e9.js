7d66283008a16bceeaf485992c1afa19a6efeaf8
felcg <xfelipecg@gmail.com>
2020-05-29 15:54:26 -0300

    Updates

---
+++ precache-manifest.60e22732e556919754e03605719b68e9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "659d57446a3507d406f903db06d9e44a",
+    "url": "/BootstrapLandingPage2/index.html"
+  },
+  {
+    "revision": "5352c11fec49cf6af693",
+    "url": "/BootstrapLandingPage2/static/css/main.c67878b5.chunk.css"
+  },
+  {
+    "revision": "0a3cff6e68042fce74e0",
+    "url": "/BootstrapLandingPage2/static/js/2.32414cf7.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/BootstrapLandingPage2/static/js/2.32414cf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5352c11fec49cf6af693",
+    "url": "/BootstrapLandingPage2/static/js/main.60a59dfa.chunk.js"
+  },
+  {
+    "revision": "1b0cb7073e04fa1ae850",
+    "url": "/BootstrapLandingPage2/static/js/runtime-main.be8b45e9.js"
+  }
+]);
